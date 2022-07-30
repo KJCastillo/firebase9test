f068a3b562e97acc1b82d7bc19f2fccf98913e06
Kevin Castillo <kevincastillo318@gmail.com>
2022-07-30 11:38:19 -0700

    connected to firebase

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ repo to learn and test google firebase 9
 - create webpack.config.js file
 - add build line to package.json
 - npm run build
+
+for vanilla js, firebase 9 steps
+- creat project, firebase.google.com
+- go to console, no need for npm firebase code
+- click on cog and scroll to bottom of general tab
+- find SDK setup and configuration
+- copy config code
+- paste to index.js (or wherever js code will be)
+- now npm install firebase
+- initialize app in index.js
+- go to firebase and then database and create
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('hello!')
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBk6MXZ_a-f7R-boYqtYftZUtKGysD9DXA",
+  authDomain: "fir-9-test-b1371.firebaseapp.com",
+  projectId: "fir-9-test-b1371",
+  storageBucket: "fir-9-test-b1371.appspot.com",
+  messagingSenderId: "866096570853",
+  appId: "1:866096570853:web:cd208c1da411414b838f0c",
+};
+
+//init firebase app
+initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+
+//collection ref
+const colRef = collection(db, "books");
+
+//get collection data
+getDocs(colRef)
+.then((snapshot) => {
+  let books = []
+  snapshot.docs.forEach((doc) => {
+    books.push({...doc.data(), id: doc.id})
+  })
+  console.log(books)
+})
+.catch(err => {
+    console.log(err.message)
+})
